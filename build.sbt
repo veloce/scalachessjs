9da629e02f98557d6b3a3dd845b070a676e58853
Vincent Velociter <vincent.velociter@gmail.com>
2016-03-29 23:22:27 +0200

    WIP on worker

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ name := "ScalachessJs"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= List(
+  "org.scala-js" %%% "scalajs-dom" % "0.9.0",
   "org.specs2" %% "specs2-core" % "3.6" % "test",
   "joda-time" % "joda-time" % "2.9.2",
   "org.scala-lang.modules" %% "scala-java8-compat" % "0.7.0",
--- src/main/scala/scalachessjs/Main.scala
+++ src/main/scala/scalachessjs/Main.scala
@@ -1,31 +1,16 @@
 package scalachessjs
 
 import scala.scalajs.js.JSApp
+import scala.scalajs.js
+import org.scalajs.dom
+import js.Dynamic.{ global => g, newInstance => jsnew, literal => obj }
+
 import chess.{ Valid, Success, Failure, Board, Game, Color, Pos }
 import chess.variant.Variant
 import chess.format.Forsyth
 
 object Main extends JSApp {
   def main(): Unit = {
-
-    def fenToGame(positionString: String, variant: Variant): Valid[Game] = {
-      val situation = Forsyth << positionString
-      (situation.map { sit =>
-        sit.color -> sit.withVariant(variant).board
-      } match {
-        case Some(sit) => chess.success(sit)
-        case None => chess.failure("Could not construct situation from FEN" )
-      }).map {
-        case (color, board) => Game(variant).copy(board = board) withPlayer color
-      }
-    }
-
-    def possibleDests(game: Game, color: Color): Map[Pos, List[Pos]] = {
-      val occ = game.board.occupation(color).toList
-      occ.map(o => o -> game.board.destsFrom(o)).collect {
-        case (p, Some(d)) if d.nonEmpty => (p, d)
-      }.toMap
-    }
 
     val game = fenToGame("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", chess.variant.Standard)
     game match {
@@ -38,5 +23,40 @@ object Main extends JSApp {
       }
     }
 
+    val self = js.Dynamic.global
+    self.addEventListener("message", { (e: dom.MessageEvent) =>
+      e.data.topic match {
+        case "dests" => {
+          val game = fenToGame(e.data.payload.fen, chess.variant.Standard)
+          self.postMessage(obj(
+            "topic" -> "dests",
+            "payload" -> obj(
+              "dests" -> possibleDests(game, game.player)
+            )
+          ))
+        }
+      }
+    })
+
   }
+
+  def fenToGame(positionString: String, variant: Variant): Valid[Game] = {
+    val situation = Forsyth << positionString
+    (situation.map { sit =>
+      sit.color -> sit.withVariant(variant).board
+    } match {
+      case Some(sit) => chess.success(sit)
+      case None => chess.failure("Could not construct situation from FEN" )
+    }).map {
+      case (color, board) => Game(variant).copy(board = board) withPlayer color
+    }
+  }
+
+  def possibleDests(game: Game, color: Color): Map[Pos, List[Pos]] = {
+    val occ = game.board.occupation(color).toList
+    occ.map(o => o -> game.board.destsFrom(o)).collect {
+      case (p, Some(d)) if d.nonEmpty => (p, d)
+    }.toMap
+  }
+
 }
